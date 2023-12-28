ccf19c8fca5f1e11104df42e0fc9971ec8cb544a
Andrew Von Stieglitz <49591230+AndrewVonStieglitz@users.noreply.github.com>
2023-12-29 09:16:35 +1100

    Update jpackage.sh script to include platform-specific packaging

--- jpackage.sh
+++ jpackage.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/bash
-jpackage --input jar/ --main-jar App.jar --main-class App --type app-image --dest dist/ --name App --icon src/icon.png --java-options -Xmx1g
-mv ./dist/App ./dist/linux
-mv ./dist/linux/bin ./dist/linux/game
+if [[ $(uname) == "Linux" ]]; then
+    jpackage --input jar/ --main-jar App.jar --main-class App --type app-image --dest dist/ --name App --icon src/icon.ico --java-options -Xmx1g
+    mv ./dist/App ./dist/linux
+    mv ./dist/linux/bin ./dist/linux/game
+fi
+
+if [[ $(uname) == "Darwin" ]]; then
+    echo "MacOS"
+fi
+
+if [[ $(uname -a) == *"CYGWIN"* ]] || [[ $(uname -a) == *"MINGW"* ]] || [[ $(uname -a) == *"MSYS"* ]]; then
+    jpackage --input jar/ --main-jar App.jar --main-class App --type app-image --dest dist/ --name App --icon src/icon.ico --java-options -Xmx1g
+    mv ./dist/App ./dist/windows
+fi
+
+
+# press any key to continue
+read -n 1 -s -r -p "Press any key to continue"
